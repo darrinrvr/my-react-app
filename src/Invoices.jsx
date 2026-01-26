b9223b1cb97cd60d9627751ff5ea7c4a860842bd
Darrin Ramroop <darrinr@rvrnetworks.com>
2026-01-26 09:18:43 -0400

    show invoice test 3

--- src/Invoices.jsx
+++ src/Invoices.jsx
@@ -2,33 +2,34 @@ import React, { useState,useEffect } from "react";
 
 export default function Invoices({token})
 {
-     return
- (
-    <>
-       <h1>Hello</h1>
-    </>
- );
-  //const [PostData,setPostData]=useState([]);
 
-//     useEffect(()=>
-//     {
-//      const fetchPost = async () => {
-//         const response = await fetch(`https://sandbox.crm.com/backoffice/v1/invoices`,
-//             {
-//           method: 'GET', // Or 'POST', 'PUT', etc.
-//           headers: {
-//             'Content-Type': 'application/json',
-//             'Authorization': `Bearer ${token}`, // Setting the Authorization header
-//           },
-//         });
+  const [PostData,setPostData]=useState([]);
+
+    useEffect(()=>
+    {
+     const fetchPost = async () => {
+        const response = await fetch(`https://sandbox.crm.com/backoffice/v1/invoices`,
+            {
+          method: 'GET', // Or 'POST', 'PUT', etc.
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`, // Setting the Authorization header
+          },
+        });
 
         
-//         const data = await response.json();
-//         setPostData(data);
-//         alert(JSON.stringify(PostData))
-//       };
-//     fetchPost();
-//  } );
+        const data = await response.json();
+        setPostData(data);
+        alert(JSON.stringify(PostData))
+      };
+    fetchPost();
+ } );
      
-
+     return
+ (
+    <div>
+       <h1>Hello</h1>
+       <button>CLICK ME</button>
+    </div>
+ );
 }
