9a9d0630e23325ae635d221efd505f9f3345b332
Darrin Ramroop <darrinr@rvrnetworks.com>
2026-01-26 09:06:17 -0400

    show invoice test

--- src/Invoices.jsx
+++ src/Invoices.jsx
@@ -4,25 +4,25 @@ export default function Invoices({token})
 {
   const [PostData,setPostData]=useState([]);
 
-    useEffect(()=>
-    {
-     const fetchPost = async () => {
-        const response = await fetch(`https://sandbox.crm.com/backoffice/v1/invoices`,
-            {
-          method: 'GET', // Or 'POST', 'PUT', etc.
-          headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${token}`, // Setting the Authorization header
-          },
-        });
+//     useEffect(()=>
+//     {
+//      const fetchPost = async () => {
+//         const response = await fetch(`https://sandbox.crm.com/backoffice/v1/invoices`,
+//             {
+//           method: 'GET', // Or 'POST', 'PUT', etc.
+//           headers: {
+//             'Content-Type': 'application/json',
+//             'Authorization': `Bearer ${token}`, // Setting the Authorization header
+//           },
+//         });
 
         
-        const data = await response.json();
-        setPostData(data);
-        alert(JSON.stringify(PostData))
-      };
-    fetchPost();
- } )
+//         const data = await response.json();
+//         setPostData(data);
+//         alert(JSON.stringify(PostData))
+//       };
+//     fetchPost();
+//  } );
      
  return
  (
